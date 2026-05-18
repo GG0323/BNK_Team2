2546bd38722f2004aa1f87506651879150eaad02
khg <gusrlf116@naver.com>
2026-05-18 17:59:29 +0900

    chore: init project

---
+++ BNK_deposit/src/main/java/com/example/bnk/BnkDepositApplication.java
@@ -0,0 +1,13 @@
+package com.example.bnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnkDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnkDepositApplication.class, args);
+	}
+
+}
---
+++ BNK_deposit/src/main/java/com/example/bnk/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.bnk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) {
+		
+		http.csrf(auth -> auth.disable());
+		
+		http.authorizeHttpRequests(request -> request
+				.requestMatchers("/").permitAll()
+		);
+		
+		return http.build();
+	}
+
+}
