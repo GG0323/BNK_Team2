6eba954f3441748192c1639c30d290e869d49acc
cherta <cherta4242@gmail.com>
2026-05-19 16:02:57 +0900

    Test 2

--- BNK_deposit/src/main/java/com/example/bnk/controller/MainController.java
+++ BNK_deposit/src/main/java/com/example/bnk/controller/MainController.java
@@ -9,5 +9,35 @@ public class MainController {
 	public String index() {
 		return "index";
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* 관리자 페이지
+	------------------------------------------------------------------------------------*/
+	
+	// 관리자 메인페이지
+	@GetMapping("/mngMain")
+	public String goManagerMainPage() {
+		return "manager/managerMain";
+	}
+	
+	// 관리자 - 제안서 보관함
+	@GetMapping("/ppsPage")
+	public String goProposalPage() {
+		return "manager/proposal";
+	}
 
 }
