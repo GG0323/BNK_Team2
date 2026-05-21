ef682cab50f2a5db5eddc300c286c0c3177302f3
GG0323 <am0777@naver.com>
2026-05-21 13:02:14 +0900

    feat: security config update

---
+++ BNK_deposit/src/main/java/com/example/bnk/auth/JwtAuthFilter.java
@@ -0,0 +1,32 @@
+package com.example.bnk.auth;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.bnk.utils.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+	
+	private final JwtUtil jwtUtil;
+	
+	// 생성자 주입
+	public JwtAuthFilter(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		
+	}
+
+}
--- BNK_deposit/src/main/java/com/example/bnk/config/SecurityConfig.java
+++ BNK_deposit/src/main/java/com/example/bnk/config/SecurityConfig.java
@@ -3,15 +3,23 @@ package com.example.bnk.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.bnk.auth.CustomLoginFailHandler;
+import com.example.bnk.auth.EmployeeLoginSuccessHandler;
+import com.example.bnk.auth.MemberLoginSuccessHandler;
+import com.example.bnk.utils.JwtUtil;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	
-	@Bean
-	BCryptPasswordEncoder passwordEncode() {
-		return new BCryptPasswordEncoder();
+	private final JwtUtil jwtUtil;
+	
+	public SecurityConfig(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
 	}
 	
 	@Bean
@@ -21,7 +29,26 @@ public class SecurityConfig {
 				.requestMatchers("/**").permitAll()
 		);
 		
+		http.formLogin(employee ->
+			employee.loginPage("/employee/loginPage")
+			.loginProcessingUrl("/employee/login")
+			.successHandler(new EmployeeLoginSuccessHandler(jwtUtil))
+			.failureHandler(new CustomLoginFailHandler())
+		);
+		
+		http.formLogin(member ->
+			member.loginPage("/member/loginPage")
+			.loginProcessingUrl("/member/login")
+			.successHandler(new MemberLoginSuccessHandler(jwtUtil))
+			.failureHandler(new CustomLoginFailHandler())
+		);
+		
 		return http.build();
+	}
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncode() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
