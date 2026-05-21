519b710e15ab6db5f66f4121bc44020f74d8983e
HYUK <dss10098@naver.com>
2026-05-21 10:59:41 +0900

    로그 검색 기능 구현

--- BNK_deposit/src/main/java/com/example/bnk/controller/page/EmployeePageController.java
+++ BNK_deposit/src/main/java/com/example/bnk/controller/page/EmployeePageController.java
@@ -1,25 +1,42 @@
 package com.example.bnk.controller.page;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.bnk.service.Employees.EmployeeLogService;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeePageController {
 	
-	@GetMapping("/toMain") // /employee/toMain
+	@Autowired
+	EmployeeLogService logService;
+	//logService.build("INSERT", "TB_EMPLOYEE", null, "신규 사원 등록 요청을 처리한다.", "POST", "/api/employee/HRM/regist");
+	
+	// /employee/toMain
+	@GetMapping("/toMain") 
 	public String mainWorkSpace() {
 		return "Employees/mainWorkspaceLogin";
 	}
 	
-
-	
-	@GetMapping("/manager/HRM/hrmRegist")  // /employee/manager/HRM/hrmRegist
+	// /employee/manager/HRM/hrmRegist
+	@GetMapping("/manager/HRM/hrmRegist")  
 	public String hrmRegist() {
+		logService.build("PAGEVIEW", null, null, "페이지간 이동을 실현한다: 인사관리/신규 사원 등록", "GET", "/employee/manager/HRM/hrmRegist");
 		return "Employees/manager/HRM/hrmRegist";
 	}
+	// /employee/manager/LOG/logList
+	@GetMapping("/manager/LOG/logList")
+	public String logList() {
+		logService.build("PAGEVIEW", null, null, "페이지간 이동을 실현한다: 로그/목록 보기", "GET", "/employee/manager/LOG/logList");
+		return "Employees/manager/LOG/logList";
+	}
+	
+	
+	
 	
 	
 	
--- BNK_deposit/src/main/java/com/example/bnk/dao/employee/IEmployeeLogDao.java
+++ BNK_deposit/src/main/java/com/example/bnk/dao/employee/IEmployeeLogDao.java
@@ -1,8 +1,12 @@
 package com.example.bnk.dao.employee;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.bnk.dto.employee.EmployeeLogDto;
 import com.example.bnk.dto.employee.EmployeeLogInsertDto;
+import com.example.bnk.dto.employee.EmployeeLogSelectDto;
 
 @Mapper
 public interface IEmployeeLogDao {
@@ -11,7 +15,10 @@ public interface IEmployeeLogDao {
 	public int insertLog(EmployeeLogInsertDto insertDto);
 	
 	
-	// 전체 로그 > 동적 쿼리
+	// 전체 로그 조회
+	public List<EmployeeLogDto> allLog();
 	
+	// 조건 검색 로그 조회 > 동적 쿼리
+	public List<EmployeeLogDto> conditionLog(EmployeeLogSelectDto selectDto);
 	
 }
