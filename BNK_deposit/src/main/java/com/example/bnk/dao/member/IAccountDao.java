d845687c53215b298a6de718170666a62c986f31
jjh <appkycard@naver.com>
2026-05-21 10:21:26 +0900

    회원의 개인정보: 이메일, 주소, 전화번호, 비밀번호 수정 로직 작성

--- BNK_deposit/src/main/java/com/example/bnk/dao/member/IAccountDao.java
+++ BNK_deposit/src/main/java/com/example/bnk/dao/member/IAccountDao.java
@@ -1,8 +1,14 @@
 package com.example.bnk.dao.member;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.bnk.dto.member.AccountDto;
 
 @Mapper
 public interface IAccountDao {
+
+	List<AccountDto> findAccountsByMemberNo(long memberNo);
 
 }
--- BNK_deposit/src/main/java/com/example/bnk/dao/member/IBankMemberDao.java
+++ BNK_deposit/src/main/java/com/example/bnk/dao/member/IBankMemberDao.java
@@ -1,8 +1,24 @@
 package com.example.bnk.dao.member;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.bnk.dto.member.BankMemberDto;
 
 @Mapper
 public interface IBankMemberDao {
 
+	public List<BankMemberDto> showMember(
+			@Param("birth_date") String brith_date, 
+			@Param("phone_number") String phone_number, 
+			@Param("member_name") String member_name
+		);
+
+	BankMemberDto findMemberById(String loginId);
+
+	void updateMemberInfo(BankMemberDto updateDto);
+
+	void updatePassword(@Param("loginId") String loginId, @Param("newPassword") String newPassword);
 }
