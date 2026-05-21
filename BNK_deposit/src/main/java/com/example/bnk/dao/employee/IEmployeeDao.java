1578a0c304e07abf1ecad668344d740c989f02b0
cherta <cherta4242@gmail.com>
2026-05-21 09:15:27 +0900

    DB와STS 연결 및 제안서, 상품, 직원 관리페이지 목록 출력

--- BNK_deposit/src/main/java/com/example/bnk/dao/employee/IEmployeeDao.java
+++ BNK_deposit/src/main/java/com/example/bnk/dao/employee/IEmployeeDao.java
@@ -1,8 +1,12 @@
 package com.example.bnk.dao.employee;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.bnk.dto.employee.EmployeeDto;
 
 @Mapper
 public interface IEmployeeDao {
-
+	public List<EmployeeDto> showAllEmp();
 }
