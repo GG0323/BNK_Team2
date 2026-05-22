4e26d570afdba5f0ff7750a916395af640143c35
cherta <cherta4242@gmail.com>
2026-05-22 18:59:53 +0900

    상품의 금리, 가입조건, 상세설명 항목을 등록 하는 페이지 만듬 및 원래있던 productDetails 페이지를 대폭 수정 - 상품의 모든 정보가 출력되게 변경

--- BNK_deposit/src/main/java/com/example/bnk/controller/page/EmployeePageController.java
+++ BNK_deposit/src/main/java/com/example/bnk/controller/page/EmployeePageController.java
@@ -1,12 +1,24 @@
 package com.example.bnk.controller.page;
 
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.bnk.dto.product.ProductConditionDto;
+import com.example.bnk.dto.product.ProductDescriptionDto;
+import com.example.bnk.dto.product.ProductDetailResponseDto;
+import com.example.bnk.dto.product.ProductDto;
+import com.example.bnk.dto.product.ProductRateDto;
 import com.example.bnk.service.employees.EmployeeLogService;
+import com.example.bnk.service.product.ProductForEmployee;
 
 @Controller
 @RequestMapping("/employee")
@@ -15,6 +27,10 @@ public class EmployeePageController {
 	@Autowired
 	EmployeeLogService logService;
 	//logService.build("INSERT", "TB_EMPLOYEE", null, "신규 사원 등록 요청을 처리한다.", "POST", "/api/employee/HRM/regist");
+	
+	@Autowired
+	private ProductForEmployee prdForEmpService;
+	
 	
 	// /employee/toMain
 	@GetMapping("/toMain") 
@@ -49,11 +65,106 @@ public class EmployeePageController {
 		return "Employees/manager/HRM/hrmEmployeeDetailList";
 	}
 		
-	// list출력
-	// /employee/staff/list
-	@GetMapping("/staff/list")
-	public String employeeProductListPage() {
+	
+	
+	/* 사원 페이지에서 상품 리스트 출력 및 관리 용도 코드 
+	-------------------------------------------------------------------------------------*/
+	// /employee/staff/product/list
+	@GetMapping("/staff/product/list")
+	public String employeeProductListPage(Model model) {
+		// 상품 리스트를 productList에 저장
+		List<ProductDto> productList = prdForEmpService.showProduct();
+		// productList를 model에 저장
+		model.addAttribute("productList", productList);
+		
 		return "Employees/staff/productList";
+	}
+	
+	
+	// 상품 디테일 페이지.
+	// /employee/prdPage/details/{product_no}
+	@GetMapping("/prdPage/detail/{product_no}")
+	public String goProductDetailPage(
+			@PathVariable("product_no") String product_no,
+			Model model) {
+	    // 상품 번호로 DB에서 상세 정보 조회
+//	    ProductDto product = prdForEmpService.showProductDetails(Integer.parseInt(product_no));
+//	    model.addAttribute("product", product_no);
+	    
+	    // 상품 번호로 DB에서 조인문을 사용한 상세 및 전체 정보 조회
+	    ProductDetailResponseDto prdDtResDto = prdForEmpService.selectProductDetail(Long.parseLong(product_no));
+	    model.addAttribute("prdDtResDto", prdDtResDto);
+	    
+	    return "Employees/staff/productDetails";
+	}
+	
+	// 상품 가입 조건 등록 페이지
+	// /employee/staff/product/write
+	@GetMapping("/staff/product/condition")
+	public String goWriteToCondition() {
+		
+		return"Employees/staff/productConditionWrite";
+	}
+	
+	// 상품 가입 조건 등록하기
+	// employee/staff/product/condition/save
+	@PostMapping("/staff/product/condition/save")
+	public String saveProductCondition(ProductConditionDto prdCndDto) {
+
+//		System.out.println(prdCndDto);
+		
+		
+		if(prdForEmpService.insertAllCondition(prdCndDto) == 1) {
+			System.out.println("성공");
+		}else {
+			System.out.println("실패");
+		}
+		
+		
+		return "redirect:/employee/staff/product/list";
+	}
+	
+	// 상품 금리 등록 페이지
+	// /employee/staff/product/rate
+	@GetMapping("/staff/product/rate")
+	public String goWriteToRate() {
+		return "Employees/staff/productRateWrite";
+	}
+	
+	// 상품 금리 등록하기
+	// employee/staff/product/rate/save
+	@PostMapping("/staff/product/rate/save")
+	public String saveProductRate(ProductRateDto prdRateDto) {
+		
+//		System.out.println(prdRateDto);
+		
+		if(prdForEmpService.insertAllRate(prdRateDto) == 1) {
+			System.out.println("성공");
+		}else {
+			System.out.println("실패");
+		}
+		return "redirect:/employee/staff/product/list";
+	}
+	
+	// 상품 설명 관리 페이지
+	// /employee/staff/product/description
+	@GetMapping("/staff/product/description")
+	public String goWriteToDescription() {
+		return "Employees/staff/productDescriptionWrite";
+	}
+	
+	// 상품 설명 관리 등록하기
+	@PostMapping("/staff/product/description/save")
+	public String saveProductDescription(ProductDescriptionDto prdDescDto) throws IOException {
+		
+		System.out.println(prdDescDto);
+		
+		if(prdForEmpService.saveDescription(prdDescDto) == 1) {
+			System.out.println("성공");
+		}else {
+			System.out.println("실패");
+		}
+		return "redirect:/employee/staff/product/description";
 	}
 	
 }
--- BNK_deposit/src/main/java/com/example/bnk/dto/product/ProductConditionDto.java
+++ BNK_deposit/src/main/java/com/example/bnk/dto/product/ProductConditionDto.java
@@ -17,7 +17,7 @@ public class ProductConditionDto {
 	private long condition_no;              // 가입조건 PK
 	private long product_no;                // 상품 FK
 	private long min_age;                   // 최소 가입 나이
-	private long max_age;                   // 최대 가입 나이
+	private Long max_age;                   // 최대 가입 나이
 	private String foreigner_available_yn;  // 외국인 가입 가능 여부
 	private String overseas_tax_yn;         // 해외 납세 의무 확인 필요 여부 DEFAULT 'Y'   Y: 확인 필요 / N: 확인 불필요
 	private String gender;                  // 성별 제한  M: 남성 / F: 여성 / NULL: 제한 없음
@@ -26,10 +26,10 @@ public class ProductConditionDto {
 	private String non_taxable_savings_yn;  // 비과세종합저축 가능 여부 DEFAULT 'N'  Y: 가능 / N: 불가
 	private String depositor_protection_yn; // 예금자보호 대상 여부 DEFAULT 'Y'     Y: 대상 / N: 비대상
 	private long min_join_amount;           // 최소 가입금액
-	private long max_join_amount;           // 최대 가입금액
-	private long deposit_unit;              // 입금 단위
+	private Long max_join_amount;           // 최대 가입금액
+	private Long deposit_unit;              // 입금 단위
 	private long min_term_months;           // 최소 가입기간
-	private long max_term_months;           // 최대 가입기간
+	private Long max_term_months;           // 최대 가입기간
 	private String term_unit_type;          // 가입기간 단위 유형 DEFAULT 'MONTH'  DAY:일 MONTH:월 YEAR:년
 	private long term_unit_value;           // 가입기간 단위 값  DEFAULT 1 예) 1개월 단위
 	private String fixed_term_yn;           // 가입기간 고정 여부 DEFAULT 'N' Y: 지정 기간만 가능 N: 기간 지정 없읔
