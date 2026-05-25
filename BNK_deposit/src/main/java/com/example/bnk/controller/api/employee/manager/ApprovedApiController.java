8130b09422194cfff0885ae0a0cb2d0b9b97c64a
HYUK <jinhyok7@gmail.com>
2026-05-25 22:33:53 +0900

    중간테이블 작업중...

--- BNK_deposit/src/main/java/com/example/bnk/controller/api/employee/manager/ApprovedApiController.java
+++ BNK_deposit/src/main/java/com/example/bnk/controller/api/employee/manager/ApprovedApiController.java
@@ -1,5 +1,6 @@
 package com.example.bnk.controller.api.employee.manager;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApprovedApiController {
 	
 	
+	
+	
+	@GetMapping("/approvedList")
+	public void approvedList() {
+		//리턴할 DTO 있어야하고 
+		//서비스
+		//
+	}
 	
 	
 
