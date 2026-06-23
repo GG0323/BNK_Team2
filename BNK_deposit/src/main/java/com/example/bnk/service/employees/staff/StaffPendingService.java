2879ddbefe25164c39e29efdab7c961aafcb8e76
cherta <cherta4242@gmail.com>
2026-06-23 15:48:43 +0900

    약관 등록 기능 완성

--- BNK_deposit/src/main/java/com/example/bnk/service/employees/staff/StaffPendingService.java
+++ BNK_deposit/src/main/java/com/example/bnk/service/employees/staff/StaffPendingService.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.bnk.dao.product.IProductRateDao;
+import com.example.bnk.dao.product.IProductTermsDao;
 import com.example.bnk.dto.product.ProductRateDto;
+import com.example.bnk.dto.product.ProductTermsDto;
 
 @Service
 public class StaffPendingService {
 
 	@Autowired
 	IProductRateDao iProductRateDao;
+	@Autowired
+	IProductTermsDao iProductTermsDao;
 	
 	// 금리 등록 서비스!
 	public int insertAllRate(ProductRateDto rateDto) {
@@ -27,5 +31,22 @@ public class StaffPendingService {
 		System.out.println("금리 등록이 완료되었습니다.");
 		return 1;
 	}
+
+	public int insertAllTerms(ProductTermsDto termsDto) {
+		int result = 0;
+		if(termsDto == null) {
+			System.out.println("입력된 약관 DTO가 NULL을 가지고 있습니다");
+			return 0;
+		}
+		result = iProductTermsDao.insertProductTerms(termsDto);
+		if(result == 0) {
+			System.out.println("DB에 입력하던 중 오류가 발생하였습니다.");
+			return 0;
+		}
+		System.out.println("약관 등록이 완료되었습니다.");
+		return 1;
+	}
 	
 }
+
+
